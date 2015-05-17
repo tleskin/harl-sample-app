4219edc7e94dd631f0fc57bc90f161d7f61a5f4f
Thomas Leskin <tleskin@gmail.com>
2015-05-17 15:10:46 -0600

    Add test for lowercase email and disallow double dots in email domain names

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -30,6 +30,14 @@ class UserTest < ActiveSupport::TestCase
     refute @user.valid?
   end
 
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
   test "email validation should accept valid emails" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
@@ -42,7 +50,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                         foo@bar_baz.com foo@bar+baz.com]
+                         foo@bar_baz.com foo@bar+baz.com foo@bar..com]
 
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
