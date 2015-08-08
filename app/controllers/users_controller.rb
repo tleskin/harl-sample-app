d741a8fabdee3ffd02b856f424f5f9c68da82a33
Thomas Leskin <tleskin@gmail.com>
2015-08-08 07:17:40 -0600

    Finish log in/log out, Chapter 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      # UserNotifier.send_signup_email(@user).deliver
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
